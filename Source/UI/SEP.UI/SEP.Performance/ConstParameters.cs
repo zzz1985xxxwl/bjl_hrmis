7d8255c3b0f3291f38fced8daae5d1c1d84cef88
zzz1985xxxwl <betterman212002@gmail.com>
2018-09-20 19:49:18 +0800

    导出修复

--- Source/UI/SEP.UI/SEP.Performance/ConstParameters.cs
+++ Source/UI/SEP.UI/SEP.Performance/ConstParameters.cs
@@ -180,7 +180,7 @@ namespace SEP.Performance
         public const string Template_AssessExportDoc = "../Template/AssessExport.doc";
         public const string Template_PositionInfoDoc = "../Template/PositionInfo.doc";
         public const string Template_EmployeeInfoDoc = "../Template/EmployeeInfo.doc";
-        public const string Template_NormalForContractDoc = "../Template/合同期满评估表.doc";
+        public const string Template_NormalForContractDoc = "../Template/合同期满评估表.docx";
         public const string Template_EmployeeNormalSummaryDoc = "../Template/员工考评个人工作总结表.docx";
         public const string Template_EmployeeAnnualSummaryDoc = "../Template/员工年终绩效考评个人工作总结表.docx";
         public const string Template_AnnualDoc = "../Template/年度员工绩效考核统计表.docx";
